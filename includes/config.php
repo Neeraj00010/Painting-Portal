2cb7046ad299961fa5a284d0efa269552fff95b0
Neeraj Bansal <90657768+Neeraj00010@users.noreply.github.com>
2022-09-07 17:53:32 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$username="portaluser";
-$password='portal@user';
-$dbName='paintingportal';
+$username="b02eed90199859";
+$password='eb84a1af';
+$dbName='us-cdbr-east-06.cleardb.net';
 $con=mysqli_connect($host,$username,$password,$dbName);
 // Check connection
 if (mysqli_connect_errno())
